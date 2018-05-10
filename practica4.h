5d31d1ff924e94f60a70cdb28fcef0f5db609c03
miguelb15 <34912093+miguelb15@users.noreply.github.com>
2018-05-10 09:03:27 +0200

    A

--- practica4.h
+++ practica4.h
@@ -1,75 +1,127 @@
-#ifndef PRACTICA4_H
-#define PRACTICA4_H
-
+#pragma once
 #include <iostream>
 
 using namespace std;
 /*
- * La clase Transportar se usa para como clase padre para
- * representar aquellos elementos que pueden ser transportados
+ * Clase Transportable
  */
-class Transportar {
-private:
-    double volumen;
-    string nombre;
+class Transportable {
+private: //atributos
+  double volumen;
+  string nombre;
 public:
-	Transportar(double vol, string nom){
-		this->volumen = vol;
-		this->nombre = nombre;
-	}
-	double getVolumen(){
-		return this->volumen;
-	}
-	void setVolumen(double vol){
-		this->volumen = vol;
-	}
-};
-//probando push
-
-class Generico : public Transportar{
-public:
-	Generico(double volumen, string nombre):
-	 		Transportar(volumen,nombre) {}
-};
-
-class Toxico : public Transportar {
-public:
-	Toxico();
+  //constructor
+  Transportable(double vol, string nom){
+    this->volumen = vol;
+    this->nombre = nom;
+  	cout << "Se ha creado un " << this->nombre;
+  }
+  //getters
+  double getVolumen() {
+      return this->volumen;
+  }
+  void getVolumen2(){
+    cout <<"con capacidad " <<getVolumen()<<".\n";
+  }
+  string getNombre() const{
+    return this->nombre;
+  }
 };
 
-class SerVivo : public Transportar {
+/*
+ * Clase Generico
+ */
+class Generico : public Transportable{
 public:
-	SerVivo();
+  //constructor
+  Generico(double vol, string nom) : Transportable(vol, nom){}
 };
-
+/*
+ * Clase Toxico
+ */
+class Toxico : public Transportable {
+public:
+  //constructor
+  Toxico(double vol, string nom) : Transportable(vol, nom) {
+  	cout <<",un TOXICO "; getVolumen2();
+  }
+};
+/*
+ * Clase SerVivo
+ */
+class SerVivo : public Transportable {
+public:
+  //constructor
+  SerVivo(double vol, string nom) : Transportable(vol, nom) {
+  	cout <<",un SER VIVO "; getVolumen2();
+  }
+};
+/*
+ * Clase Producto
+ */
 class Producto : public Generico {
 public:
-	Producto(double volumen, string nombre) : 
-		Generico(volumen, nombre) {}
+  //constructor
+  Producto(double vol, string nom) : Generico(vol, nom){
+  	cout <<",un PRODUCTO "; getVolumen2();
+  }
 };
-
-class Guardar {
-private:
-    double capacidad;
+/*
+ * Clase Guardable
+ */
+class Guardable {
+private: //atributos
+  double capacidad;
 public:
-	Guardar(double cap) : capacidad(cap);
-	double getCapacidad(){
-		return this->capacidad;
-	}
-	void setCapacidad(){
-		this->capacidad = cap;
-	}
+  //constructor
+  Guardable(double cap) : capacidad(cap) {}
+  double getCapacidad() {
+      return this->capacidad;
+  }
+  //funciones
+  void actualizarCapacidad(double vol) {
+    this->capacidad -= vol;
+  }
+  template<typename T>
+  bool guardar(T elemento) {
+    if (elemento.getVolumen()  > getCapacidad() ) { //si no cabe
+      return false;                                 //devuelve false
+    }else{ //si cabe, actualiza la capidad e informa por pantalla
+      actualizarCapacidad(elemento.getVolumen());
+      cout<<"Guardado elemento de cap. "<<elemento.getVolumen() <<". ";
+      cout<<"Capacidad disponible: "<<getCapacidad()<<".\n";
+      return true; // y devuelve true
+    }
+  }
 };
-
-class Camion : public Guardar {
+/*
+ * Clase Camion
+ */
+class Camion : public Guardable {
 public:
-	Camion(double cap) : Guardar(capacidad) {}
+  //constructor
+  Camion(double cap) : Guardable(cap) {
+    cout<<"Se ha creado un CAMION con capacidad " << cap << ". \n";
+  }
+  //funciones
+  bool guardar(Generico elemento) {
+    cout << "CAMION: ";
+    return Guardable::guardar(elemento);
+  }
 };
-
+/*
+ * Clase Contenedor
+ */
 template<class T>
-class Contenedor : public Guardar, public Generico {
+class Contenedor : public Guardable, public Generico {
 public:
-	Contenedor();
+  //constructor
+  Contenedor(double cap) : Guardable(cap), Generico(cap,"") {
+  	cout<< "CONTENEDOR con capacidad " << cap << ". \n";
+  }
+  //funciones
+  bool guardar(T elemento) {
+    cout << "CONTENEDOR: ";
+    return Guardable::guardar(elemento);
+  }
 };
-
-#endif //PRACTICA4_H
