a17d163c6bcb43febb0dec4f9d920b4cb5191d22
miguelb15 <34912093+miguelb15@users.noreply.github.com>
2018-05-10 09:04:16 +0200

    A

--- practica4.h
+++ practica4.h
@@ -4,17 +4,32 @@
 #include <iostream>
 
 using namespace std;
+/*
+ * La clase Transportar se usa para como clase padre para
+ * representar aquellos elementos que pueden ser transportados
+ */
 class Transportar {
+private:
     double volumen;
     string nombre;
 public:
-	Transportar();
+	Transportar(double vol, string nom){
+		this->volumen = vol;
+		this->nombre = nombre;
+	}
+	double getVolumen(){
+		return this->volumen;
+	}
+	void setVolumen(double vol){
+		this->volumen = vol;
+	}
 };
 //probando push
 
 class Generico : public Transportar{
 public:
-	Generico();
+	Generico(double volumen, string nombre):
+	 		Transportar(volumen,nombre) {}
 };
 
 class Toxico : public Transportar {
@@ -29,18 +44,26 @@ public:
 
 class Producto : public Generico {
 public:
-	Producto(double volumen, String nombre) : volumen(volumen), nombre(nombre) {}
+	Producto(double volumen, string nombre) : 
+		Generico(volumen, nombre) {}
 };
 
 class Guardar {
+private:
     double capacidad;
 public:
-	Guardar();
+	Guardar(double cap) : capacidad(cap);
+	double getCapacidad(){
+		return this->capacidad;
+	}
+	void setCapacidad(){
+		this->capacidad = cap;
+	}
 };
 
 class Camion : public Guardar {
 public:
-	Camion(int capacidad) : capacidad(capacidad) {}
+	Camion(double cap) : Guardar(capacidad) {}
 };
 
 template<class T>
